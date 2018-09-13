a6e25371f9b096b50499365f2f59aa15f8eb4f76
siuwaiyuen <763137435@qq.com>
2018-09-13 22:12:29 +0800

    实现-a功能

--- wc/wc.cpp
+++ wc/wc.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void baseCount(char *filename, int *cnt)
+void basicCount(char *filename, int *cnt)
 {
 	char ch;
 	char lch;
@@ -27,12 +27,12 @@ void baseCount(char *filename, int *cnt)
 		{
 			mark = (ch == ' ' || ch == ';' || ch == ',' || ch == '\t' || ch == '\n' || ch == '(' || ch == ')');
 			if (mark && wFlag)
-				cnt[1]++;   //统计词数 
+				cnt[1]++;   //word 
 			wFlag = !mark;
 			if (isgraph(ch))
-				cnt[0]++;   //统计字符数
+				cnt[0]++;   //character
 			if (ch == '\n')
-				cnt[2]++; //统计行数
+				cnt[2]++; //line
 			lch = ch;
 		}
 		inFile >> ch;
@@ -42,11 +42,106 @@ void baseCount(char *filename, int *cnt)
 		cnt[2]++;
 	return;
 }
+void extendedCount(char *filename, int *cnt)
+{
+	string line;
+	int flagQuotes = 0, flagSlah = 0, flagCode = 0, flagComment = 0;
+	int i, nBoth = 0;
+	fstream inFile;
+	inFile.open(filename);
+	if (!inFile.is_open())
+	{
+		cerr << "Open Failed." << endl;
+		exit(1);
+	}
+	while (!inFile.eof())
+	{
+		getline(inFile, line);
+		i = 0;
+		flagCode = 0;
+		flagComment = 0;
+		if (flagSlah)
+			flagComment = 1;
+		while (line[i] == ' ' || line[i] == '\t' || line[i] == '\r' || line[i] == '\n')
+			++i;
+		if (!flagSlah && (line[i] == '\0' || line[i + 1] == '\0'))
+		{
+			cnt[3]++;
+			continue;
+		}
+		while (1)
+		{
+			if (!flagSlah && line[i] == '/' && line[i + 1] == '/')
+			{
+				if (flagCode)
+				{
+					cnt[4]++;     //comment
+					cnt[5]++;     //code
+					nBoth++;     //code & comment
+				}
+				else          //only comment
+				{
+					cnt[4]++;
+				}
+				break;
+			}
+			//Multi-line comments start
+			if (!flagSlah && line[i] == '/' && line[i + 1] == '*')
+			{
+				i += 2;
+				flagSlah = 1;
+				flagComment = 1;
+				continue;
+			}
+			//Multi-line comments
+			if (flagSlah)
+			{
+				//"*/End of comment"
+				if (line[i] == '*' && line[i + 1] == '/')
+				{
+					i++;
+					flagSlah = 0;
+				}
+				else if (line[i] == '\0')
+				{
+					if (flagCode)
+					{
+						cnt[4]++;
+						cnt[5]++;
+						nBoth++;
+					}
+					else
+					{
+						++cnt[4];       //only comment
+					}
+					break;
+				}
+				++i;
+				continue;
+			}
+			if (line[i] == '\0')
+			{
+				if (flagComment)
+					cnt[4]++;
+				if (flagCode)
+					cnt[5]++;
+				if (flagComment && flagCode)
+					nBoth++;
+				break;
+			}
+			i++;
+			flagCode = 1;
+		}
+
+	}
+	inFile.close();
+}
 
 int main(int argc, char* argv[])
 {
-	int cnt[3] = { 0 };
-	baseCount(argv[argc - 1], cnt);
+	int cnt[6] = { 0 };
+	basicCount(argv[argc - 1], cnt);
+	extendedCount(argv[argc - 1], cnt);
 	for (int i = 1; i < argc - 1; i++)
 	{
 		if (!strcmp(argv[i], "-c"))
@@ -55,6 +150,8 @@ int main(int argc, char* argv[])
 			cout << "words:" << cnt[1] << endl;
 		if (!strcmp(argv[i], "-l"))
 			cout << "lines:" << cnt[2] << endl;
+		if (!strcmp(argv[i], "-a"))
+			cout << "blank lines:" << cnt[3] << endl << "comment lines:" << cnt[4] << endl << "code lines:" << cnt[5] << endl;
 	}
 	system("pause");
 	return 0;
@@ -63,4 +160,3 @@ int main(int argc, char* argv[])
 	return 0;
 
 }
-
